592ba821063c96f0c65f57ab000e95934c7d658b
reateguiLumisa <62541772+reateguiLumisa@users.noreply.github.com>
2022-08-29 10:11:52 +0200

    RA-23

---
+++ controllers/puntoConexion.controller.js
@@ -0,0 +1,63 @@
+const PuntoConexion = require('../models/PuntoConexion.model');
+const createError = require('http-errors');
+
+const list = (req, res, next) => {
+
+    PuntoConexion.find()
+    .then(item => {
+        res.json(item);
+    })
+    .catch(next);
+}
+
+const create = (req, res, next) => {
+    
+    const data = req.body;
+
+    PuntoConexion.create({
+        ...data,
+    })
+    .then(item => {
+        res.status(201).json(item);
+    })
+    .catch(next)
+}
+
+const detail = (req, res, next) => {
+    PuntoConexion.findById(req.params.id)
+    .then(item => {
+
+        if (item) {
+            res.json(item);
+        } else {
+            next(createError(404, 'item not found'));
+        }
+
+    })
+}
+
+const update = (req, res, next) => {
+    const body = {title, address, description, phoneNumber} = req.body;
+
+    PuntoConexion.findByIdAndUpdate(req.params.id, body, { new: true }).then( item => {
+        if (item) {
+            res.json(item);
+        } else {
+            next(createError(404, 'item not found'));
+        }
+    })
+}
+
+const remove = (req, res, next) => {
+    PuntoConexion.findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).send())
+    .catch(next);
+}
+
+module.exports = {
+    list,
+    create,
+    detail,
+    update,
+    remove
+}
